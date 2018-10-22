84c2672e918722768b3fbe43e3e563040d73166f
Yevheniia Velychko <yevheniia.velychko@synapse.com>
2018-10-22 14:19:06 +0300

    Working tests for User Controller

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,7 @@ module.exports = function (config) {
             '../node_modules/jquery/dist/jquery.js',
             '../node_modules/angular/angular.js',
             '../node_modules/angular-mocks/angular-mocks.js',
-            '**/*.html',
+            '**/*.html',git
             '**!/!*.template.html',
             './!**!/!*.js'
         ],
